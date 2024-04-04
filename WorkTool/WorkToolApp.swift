84b3ef4db05644988a0d1ca33a6e5845b0b9a4bb
louisng <ziyao0428@hotmail.com>
2024-04-04 22:20:36 +1300

    Initial Commit

---
+++ WorkTool/WorkToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkToolApp.swift
+//  WorkTool
+//
+//  Created by Louis on 04/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WorkToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
