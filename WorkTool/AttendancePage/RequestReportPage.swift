3ef6a15038638dfc930adc2655ce6da726bbfece
LouisDeveloper <louisngdeveloper@gmail.com>
2024-04-19 17:05:01 +1200

    commit

---
+++ WorkTool/AttendancePage/RequestReportPage.swift
@@ -0,0 +1,18 @@
+//
+//  RequestReportPage.swift
+//  WorkTool
+//
+//  Created by Louis on 16/04/2024.
+//
+
+import SwiftUI
+
+struct RequestReportPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RequestReportPage()
+}
